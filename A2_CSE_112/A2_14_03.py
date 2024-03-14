c55d92f619095a31f4842ac809be43b187cac859
ABC <mayukc55@gmail.com>
2024-03-14 20:26:53 +0530

    a2 112 14_03_24

---
+++ A2_CSE_112/A2_14_03.py
@@ -0,0 +1,28 @@
+# for i in range(10):
+#     print(i)
+# for i in range(10):
+#     print("Hi")
+
+# for i in range(5,21):
+#     print(i)
+
+# for i in range(5,21,2):
+#     print(i)
+
+# s="Mayukhjit"
+#
+# for var in s:
+#     print(var)
+
+
+# for m in range(2,10,2):
+#     print(m)
+
+
+s="my name is Mayukhjit"
+v="aeiouAEIOU"
+for i in s:
+    if i in v:
+        print(i)
+
+
